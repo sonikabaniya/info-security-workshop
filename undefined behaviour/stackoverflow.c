0b81c186a47d86b2bed26ff308750af4344f3100
Sonika Baniya <sonika.baniya.sb@gmail.com>
2024-04-25 19:50:58 +0300

    undefined behaviour example

---
+++ undefined behaviour/stackoverflow.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int examplearray[3];
+
+    for (int i = 0; i < 7; i++){
+        examplearray[i] = i;
+    }
+
+    return 0;
+}
---
+++ undefined behaviour/typeconfusion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int sthnumber;
+    printf("Enter some integer number");
+
+    scanf("%d", &sthnumber);
+    printf("The entered value is %d", sthnumber);
+
+
+    return 0;
+}
